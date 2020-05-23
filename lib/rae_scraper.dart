c1475a7e3e3af320b5cd20ba73f44f7062832cce
Foo-Manroot <foomanroot@disroot.org>
2020-05-23 16:35:27 +0200

    Exportados los tipos 'Definic' y 'Expr'

--- lib/rae_scraper.dart
+++ lib/rae_scraper.dart
@@ -2,6 +2,8 @@
 export 'src/scraper.dart';
 export 'src/modelos/enums.dart';
 export 'src/modelos/Acepc.dart';
+export 'src/modelos/Definic.dart';
 export 'src/modelos/Entrada.dart';
+export 'src/modelos/Expr.dart';
 export 'src/modelos/Palabra.dart';
 export 'src/modelos/Resultado.dart';
