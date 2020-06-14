a4371989e665a8b1bce27f8f208555bd02a7d1e5
Foo-Manroot <foomanroot@disroot.org>
2020-06-14 21:14:21 +0200

    Ejemplo actualizado

--- example/cli_scraper.dart
+++ example/cli_scraper.dart
@@ -36,48 +36,19 @@ void main (List<String> argv) async {
                 ;
                 print (a.toString ());
 
-                await a.obtenerDef (
+                print (await a.obtenerSugerencias ("a"));
+
+                print (await a.palabraDelDia ());
+
+                a.obtenerDef (
                     argv [0],
                     manejadorExcepc: (e) => print ("Excepción en petic. 1 => $e"),
                     manejadorError: (e) => print ("Excepción en petic. 1 => $e")
                 ).then (
                     (Resultado res) => (res == null)? null :  res.mostrarResultados ()
+                ).whenComplete (
+                    () => a.dispose ()
                 );
-
-                for (int i = 0; i <= 4; i++)  {
-                    /* Cache hit */
-                    await a.obtenerDef (
-                        "gato",
-                        manejadorExcepc: (e) => print ("Excepción en petic. 1 => $e"),
-                        manejadorError: (e) => print ("Excepción en petic. 1 => $e")
-//                  ).then (
-//                      (Resultado res) => (res == null)? null :  res.mostrarResultados ()
-//                ).whenComplete (
-//                    () => a.dispose ()
-                    );
-                }
-
-                /********************/
-                /* Segunda petición */
-                /********************/
-
-                Palabra p = Palabra (
-                      "asdf", dataId: "XhbjsNo#ETrKQH3"
-                );
-
-                for (int i = 0; i <= 4; i++)  {
-                    await p.obtenerDef (a);
-                }
-
-                for (int i = 0; i <= 4; i++)  {
-                    await a.obtenerDef ("asdf");
-                }
-
-                for (int i = 0; i <= 4; i++)  {
-                    await a.obtenerDef ("pepinillo").whenComplete (
-                        () => a.dispose ()
-                    );
-                }
         }
 
     } else {
